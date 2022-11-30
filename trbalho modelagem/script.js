86867e1ebfa39923abb073a4d63eca4e17244c32
adonis886 <114776685+adonis886@users.noreply.github.com>
2022-11-30 16:47:41 -0300

    Add files via upload

---
+++ trbalho modelagem/script.js
@@ -0,0 +1,13 @@
+function abrirmenu(){
+    let barramenuaberto = document.getElementById('barra-menu');
+
+    if(barramenuaberto.style.width == "0px"){
+        barramenuaberto.style.width = "180px";
+    
+    }
+else{
+    barramenuaberto.style.width = "0px";
+}
+
+
+}
